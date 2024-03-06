580451887c728b4204a2941dadc87bbfafc74eb2
Warren <thewarrenbarton@gmail.com>
2024-03-06 14:07:23 +0000

    more improvements

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div className='header field'>
-      Warren's awesome VAT calculator
+      Warren's awesome Value Added Tax calculator
       <div className='colour-border'>
         <VatRateField customstyle="field" vatRateChanged={handleVatRateChanged} value={vatRate} updatePrices={updatePrices} />
         <PriceEntryField customstyle="field" label="Price excl VAT: " priceChanged={handleNetPriceChange} price={netPrice === 0.0 ? "" : netPrice} />
